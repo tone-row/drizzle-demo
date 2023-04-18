0b9b467313b64f4c7528df93633245013f283d85
Rob Gordon <rchristopher.gordon@gmail.com>
2023-04-18 17:00:59 -0400

    Add created_at column to user table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,9 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 // schema.ts
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   fullName: text("full_name"),
   phone: varchar("phone", { length: 256 }),
+  createdAt: timestamp("created_at"),
 });
