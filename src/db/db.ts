1baa058679dd6ea4f266f0ad594919b58f6302ef
Rob Gordon <rchristopher.gordon@gmail.com>
2023-04-18 16:40:13 -0400

    Add files

---
+++ src/db/db.ts
@@ -0,0 +1,14 @@
+import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+import { users } from "./schema";
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool);
